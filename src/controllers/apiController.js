ba9621e963d56ac13998a97e181cb9d69d0c0f61
HaoLong4935 <longsachse@gmail.com>
2023-05-02 16:32:59 +0700

    Video 76

--- src/controllers/apiController.js
+++ src/controllers/apiController.js
@@ -27,5 +27,30 @@ const postCreateUserAPI = async (req, res) => {
     });
 }
 
+const puttUpdateUserAPI = async (req, res) => {
+    console.log("Request body:", req.body);
 
-module.exports = { getUsersAPI, postCreateUserAPI }
+    //Lấy các giá trị người dùng nhập trong req.body gửi lên server
+    let email = req.body.emailID;
+    let name = req.body.nameInput;
+    let city = req.body.cityInput;
+    //Với update chúng ta cần thêm một tham số nữa là Id
+    let userId = req.body.userId;
+
+    // await User.updateOne({ điều kiện để filter(lọc) }, { các giá trị muốn cập nhật }) => Trả về một {};
+    let user = await User.updateOne({ _id: userId }, { email: email, name: name, city: city });
+
+    console.log(">> emial=", email, " name=", name, " city=", city, " userId=", userId)
+    // let [results, fields] = await connection.query(
+    //     `INSERT INTO Users(email,name,city)VALUES(?,?,?) `, [email, name, city],
+    // );
+    //res.send("Update Success")
+    return res.status(200).json({
+        errorCode: 0,
+        data: user
+    });
+    //const [results, fields] = await connection.query('SELECT * FROM Users u');
+}
+
+
+module.exports = { getUsersAPI, postCreateUserAPI, puttUpdateUserAPI }
--- src/routes/api.js
+++ src/routes/api.js
@@ -2,10 +2,11 @@ const express = require('express');
 
 const routerAPI = express.Router();
 
-const { getUsersAPI, postCreateUserAPI } = require('../controllers/apiController')
+const { getUsersAPI, postCreateUserAPI, puttUpdateUserAPI } = require('../controllers/apiController')
 
 
 routerAPI.get('/users', getUsersAPI);
 routerAPI.post('/users', postCreateUserAPI);
+routerAPI.put('/users', puttUpdateUserAPI);
 
 module.exports = routerAPI;
